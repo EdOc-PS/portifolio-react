954495c13eadaba693cd40bba981e7f2c01c64aa
Edoc-ps <eeuardooctavio@gmail.com>
2026-02-22 12:03:31 -0300

    feat: implement loading skeletons for projects and modal; update header links

---
+++ src/components/ui/Loading/ProjectSkeleton/index.tsx
@@ -0,0 +1,11 @@
+const ProjectSkeleton = () => {
+    return (
+
+        <div className="mb-4 space-y-2">
+            <div className="h-14.25 w-60 rounded-xl skeleton-shimmer" />
+        </div>
+
+    )
+}
+
+export default ProjectSkeleton;
